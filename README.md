7aaee226ed0871d586b59c3dc374983e012b04de
lapwat <q.lapointe@gmail.com>
2021-01-25 23:54:16 +0100

    better readme, extract title and chapters from pages

--- README.md
+++ README.md
@@ -4,9 +4,12 @@ Turn a website into a book.
 
 ## Usage
 
+Run this command.
+
 ```sh
 node index.js BASE_URL HOME_PATH NAVITEM_SELECTOR OUT_DIR
 # i.e. node index.js https://wiki.polkadot.network /docs/en/getting-started .navItem out
+# i.e. node index.js https://dev.to / 'a[id*="article-link-"]' out
 ```
 
 - `BASE_URL` Base url of the website, without trailing slash
@@ -14,12 +17,12 @@ node index.js BASE_URL HOME_PATH NAVITEM_SELECTOR OUT_DIR
 - `NAVITEM_SELECTOR` Selector for links on the HOME_PAGE to build the table of content
 - `OUT_DIR` Where the downloaded sources will be located
 
+Then import $OUT_DIR/final.html into Calibre by clicking **Add books**.
+
+Click **Convert books**, go to **Structure Detection** tab and set _Detect Chapters_ and _Insert page breaks_ value to `//h:h1[re:test(@class, "chapter", "i")]`.
+
 ## Todo
 
-- [] Add title argument
-- [] Default values for arguments
-- [] Named arguments
-- [] Trim urls
-- [] Use website title for ebook title
-- [] Use subpages titles for chapters titles 
-- [] Better README
+- [ ] Default values for arguments
+- [ ] Named arguments
+- [ ] Trim urls
--- index.js
+++ index.js
@@ -10,51 +10,39 @@ const domain = myArgs[0];
 const index = myArgs[1]; 
 const navSelector = myArgs[2]; 
 const outDir = myArgs[3];
+const absoluteOutFile = path.join(outDir, 'final.html')
 const body = request('GET', domain + index).getBody();
 const dom = new JSDOM(body);
+const title = dom.window.document.querySelector('title').textContent;
 const tableOfContent = dom.window.document.querySelectorAll(navSelector);
 
-const urls = []
+let urls = []
 for (const item of tableOfContent) {
   urls.push({
     url: domain + item.href,
-    filename: path.basename(item.href) + '.html',
+    filename: path.basename(item.href),
   });
-  console.log(item.href)
 }
 
+console.log(`Links in table of content: ${urls.length}`);
+
 (async () => {
-   await scrape({
-     urls,
-     urlFilter: url => url.startsWith(domain),
-     directory: outDir,
+  await scrape({
+    urls,
+    urlFilter: url => url.startsWith(domain),
+    directory: outDir,
   });
 
+  fs.appendFileSync(absoluteOutFile, `<title>${title}</title>`);
+ 
   for (const url of urls) {
-    const absoluteFilename = path.join(outDir, url.filename);
+    const absoluteFilename = path.join(outDir, `${url.filename}.html`);
     const body = fs.readFileSync(absoluteFilename);
     const dom = new JSDOM(body);
     const reader = new Readability(dom.window.document);
     const article = reader.parse();
 
-    fs.appendFileSync(path.join(outDir, 'final.html'), article.content);
+    fs.appendFileSync(path.join(outDir, 'final.html'), `<h1 class="chapter">${article.title}</h1>${article.content}`);
   }  
 
 })();
-
-
- // const body2 = request('GET', 'https://wiki.polkadot.network' + item.href).getBody();
- // const dom2 = new JSDOM(body2);
- // const reader = new Readability(dom2.window.document);
- // const article = reader.parse();
-
- // //fs.appendFileSync('out.html', article.content);
- // scrape({
- //   //urls: ['https://wiki.polkadot.network' + item.href],
- //   urls: ['https://wiki.polkadot.network/docs/en/getting-started',
-//'//https://wiki.polkadot.network/docs/en/claims'
-//,
- //   directory: 'out',
- // }).then((result) => console.log(result));
- // break;
-//
