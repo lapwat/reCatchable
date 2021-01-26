baa3a347e10adddcdf19580ffa536bc53531208b
lapwat <q.lapointe@gmail.com>
2021-01-27 00:14:54 +0100

    better readme, help, named args, default args

--- README.md
+++ README.md
@@ -12,25 +12,34 @@ yarn
 
 ## Usage
 
-Run this command.
-
 ```sh
-node index.js BASE_URL HOME_PATH NAVITEM_SELECTOR OUT_DIR
-# i.e. node index.js https://wiki.polkadot.network /docs/en/getting-started .navItem out
-# i.e. node index.js https://dev.to / 'a[id*="article-link-"]' out
+Usage: ./index.js [options]
+
+Options:
+      --help      Show help                                                                                              [boolean]
+      --version   Show version number                                                                                    [boolean]
+  -b, --base-url  Base url of the website, without trailing slash                                              [string] [required]
+  -s, --selector  HTML selector for links on the HOME page to build the table of content                       [string] [required]
+  -h, --home      Path  of the website where the table of content is located, with leading slash           [string] [default: "/"]
+  -o, --out-dir   Folder where final.html will be downloaded                                             [string] [default: "out"]
+
+Examples:
+  ./index.js -b https://wiki.polkadot.network -h /docs/en/getting-started -s .navItem
+  ./index.js -b https://dev.to -s 'a[id*="article-link-"]'
 ```
 
-- `BASE_URL` Base url of the website, without trailing slash
-- `HOME_PATH` Path  of the website where the table of content is located, with leading slash
-- `NAVITEM_SELECTOR` Selector for links on the HOME_PAGE to build the table of content
-- `OUT_DIR` Where the downloaded sources will be located
+Output directory should not exist.
 
-Then import $OUT_DIR/final.html into Calibre by clicking **Add books**.
+## Use calibre to convert the resulting HTML to epub
+
+In Calibre,
+
+Click **Add books** and select `out/final.html`.
 
 Click **Convert books**, go to **Structure Detection** tab and set _Detect Chapters_ and _Insert page breaks_ value to `//h:h1[re:test(@class, "chapter", "i")]`.
 
+Click **Ok**.
+
 ## Todo
 
-- [ ] Default values for arguments
-- [ ] Named arguments
 - [ ] Trim urls
--- index.js
+++ index.js
@@ -1,48 +1,84 @@
+#!/usr/bin/env node
+
+// imports
 const fs = require('fs');
 const path = require('path');
 const JSDOM = require('jsdom').JSDOM;
 const { Readability } = require('@mozilla/readability');
 const request = require('sync-request');
 const scrape = require('website-scraper');
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 
-const myArgs = process.argv.slice(2);
-const domain = myArgs[0]; 
-const index = myArgs[1]; 
-const navSelector = myArgs[2]; 
-const outDir = myArgs[3];
-const absoluteOutFile = path.join(outDir, 'final.html')
-const body = request('GET', domain + index).getBody();
+// parse args
+const options = yargs(hideBin(process.argv))
+  .usage('Usage: ./$0 [options]')
+  .example('./$0 -b https://wiki.polkadot.network -h /docs/en/getting-started -s .navItem')
+  .example('./$0 -b https://dev.to -s \'a[id*="article-link-"]\'')
+  .option('base-url', {
+    alias: 'b',
+    type: 'string',
+    required: true,
+    description: 'Base url of the website, without trailing slash',
+  })
+  .option('selector', {
+    alias: 's',
+    type: 'string',
+    required: true,
+    description: 'HTML selector for links on the HOME page to build the table of content',
+  })
+  .option('home', {
+    alias: 'h',
+    type: 'string',
+    default: '/',
+    description: 'Path  of the website where the table of content is located, with leading slash',
+  })
+  .option('out-dir', {
+    alias: 'o',
+    type: 'string',
+    default: 'out',
+    description: 'Folder where final.html will be downloaded',
+  })
+  .strict()
+  .wrap(130)
+  .argv
+
+// retrieve table of content
+const body = request('GET', options.baseUrl + options.home).getBody();
 const dom = new JSDOM(body);
 const title = dom.window.document.querySelector('title').textContent;
-const tableOfContent = dom.window.document.querySelectorAll(navSelector);
+const tableOfContent = dom.window.document.querySelectorAll(options.selector);
+const absoluteOutFile = path.join(options.outDir, 'final.html')
 
+// prepare urls
 let urls = []
 for (const item of tableOfContent) {
   urls.push({
-    url: domain + item.href,
+    url: options.baseUrl + item.href,
     filename: path.basename(item.href),
   });
 }
 
-console.log(`Links in table of content: ${urls.length}`);
+if (options.verbose)
+  console.log(`Links in table of content: ${urls.length}`);
 
 (async () => {
   await scrape({
     urls,
-    urlFilter: url => url.startsWith(domain),
-    directory: outDir,
+    urlFilter: url => url.startsWith(options.baseUrl),
+    directory: options.outDir,
   });
 
   fs.appendFileSync(absoluteOutFile, `<title>${title}</title>`);
  
   for (const url of urls) {
-    const absoluteFilename = path.join(outDir, `${url.filename}.html`);
+    const absoluteFilename = path.join(options.outDir, `${url.filename}.html`);
     const body = fs.readFileSync(absoluteFilename);
     const dom = new JSDOM(body);
     const reader = new Readability(dom.window.document);
     const article = reader.parse();
 
-    fs.appendFileSync(path.join(outDir, 'final.html'), `<h1 class="chapter">${article.title}</h1>${article.content}`);
+    fs.appendFileSync(path.join(options.outDir, 'final.html'), `<h1 class="chapter">${article.title}</h1>${article.content}`);
   }  
 
 })();
