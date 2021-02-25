1be9b38236d2f795248510ed75a30500e8e4ec72
lapwat <quentin@lapw.at>
2021-02-25 15:28:51 +0100

    default export function, skip image download by scraper

--- index.js
+++ index.js
@@ -14,17 +14,29 @@ const scraper = require('./utils/scraper');
 const remarkable = require('./utils/remarkable');
 
 (async () => {
+  await run(options);
+})();
+
+async function run(options) {
   let { title, chapterUrls } = scraper.getBookStructure(options.home.origin, options.home.pathname, options.selector, options.homeHtml);
 
   title = options.title || title;
   chapterUrls = options.limit ? chapterUrls.slice(0, options.limit) : chapterUrls;
-  
+
   console.log(`Found ${chapterUrls.length} pages in the table of content.`);
   console.log(`Downloading ${title}...`);
 
   const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'cachable-')) + '/out';
-  await scraper.downloadUrls(chapterUrls, options.home.origin, tmpDir);
-  const chapters = scraper.getPagesContent(chapterUrls, tmpDir);  
+  await scraper.downloadUrls(chapterUrls, tmpDir);
+
+  if (fs.existsSync(`${tmpDir}/images`))
+    console.log('Warning: Found /images in content folder, images inside this directory may be redownloaded by epub-gen.');
+
+  console.log(`Extracting clean content from HTML sources...`);
+  const chapters = scraper.getPagesContent(chapterUrls, tmpDir);
+  fs.rmSync(tmpDir, { recursive: true, force: true });
+
+  console.log(`Creating book...`);
 
   const bookFile = `${title}.epub`;
   const epubOptions = {
@@ -34,10 +46,7 @@ const remarkable = require('./utils/remarkable');
     content: chapters.map(c => ({ title: c.title, data: c.content })),
   };
 
-  console.log(`Creating book...`);
-
   await new Epub(epubOptions, bookFile).promise;
-  fs.rmSync(tmpDir, { recursive: true, force: true });
 
   console.log(`+ Book saved to ${bookFile}`);
 
@@ -49,4 +58,6 @@ const remarkable = require('./utils/remarkable');
       console.log('Could not upload book, book name may already exists.', err);
     }
   }
-})();
+}
+
+exports.default = run;
--- utils/scraper.js
+++ utils/scraper.js
@@ -8,10 +8,15 @@ const scrape = require('website-scraper');
 
 class WebsiteScraperPlugin {
   apply(registerAction) {
-    registerAction('getReference', ({ resource, parentResource, originalReference }) => {
-      if (!resource)
-        return { reference: parentResource.url + originalReference };
-      return { reference: resource.url };
+    registerAction('getReference', ({ parentResource, originalReference }) => {
+      const absoluteReference = originalReference.startsWith('http')
+        ? originalReference
+        : parentResource.url + originalReference;
+
+      console.log('Setting absolute reference for image:', originalReference);
+      console.log('->', absoluteReference);
+
+      return { reference: absoluteReference };
     });
   }
 }
@@ -33,7 +38,9 @@ exports.getBookStructure = (origin, pathname, selector, html = null) => {
 
   let chapterUrls = []
   for (const item of tableOfContent) {
-    const url = item.href.startsWith('/') ? origin + item.href : item.href;
+    const url = item.href.startsWith('/')
+      ? origin + item.href
+      : item.href;
     const filename = path.basename(item.href).replace('?', '_') || 'index';
 
     chapterUrls.push({ url, filename });
@@ -42,10 +49,23 @@ exports.getBookStructure = (origin, pathname, selector, html = null) => {
   return { title, chapterUrls };
 }
 
-exports.downloadUrls = async (urls, origin, directory) => {
+exports.downloadUrls = async (urls, directory) => {
+  const imageExtensions = ['jpg', 'jpeg', 'png'];
+
   await scrape({
     urls,
-    urlFilter: url => url.startsWith(origin),
+    urlFilter: url => {
+      url = new URL(url);
+      const extension = url.pathname.split('.').pop().toLowerCase();
+
+      if (imageExtensions.includes(extension)) {
+        console.log('Skipping image:', url.pathname);
+        return false;
+      }
+
+      return true;
+    },
+    maxRecursiveDepth: 1,
     directory,
     sources: [{ selector: 'img', attr: 'src' }],
     plugins: [ new WebsiteScraperPlugin() ],
@@ -56,15 +76,16 @@ exports.getPagesContent = (urls, downloadDirectory) => {
   const pages = [];
 
   for (const [ index, url ] of urls.entries()) {
-    const filename = /\.html?$/.test(url.filename)
+    const filename = url.filename.endsWith('.html')
       ? url.filename
       : `${url.filename}.html`;
     const absoluteFilename = path.join(downloadDirectory, filename);
+
     const body = fs.readFileSync(absoluteFilename);
     const dom = new JSDOM(body);
     const reader = new Readability(dom.window.document);
     const { title, content } = reader.parse();
-                                                                        
+
     console.log(`+ Page ${index+1}: ${title}`);
     pages.push({ title, content });
   }
